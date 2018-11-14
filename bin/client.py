8f8e0e2800142b712e1a0896bae4a6dd4ef011da
yurun <miao.work2010@gmail.com>
2018-11-14 21:47:47 +0800

    fix: upgrade bin/client.py, bin/cosmos.py feat: update version to 0.1.2

--- bin/client.py
+++ bin/client.py
@@ -2,19 +2,41 @@
 # -*- coding: utf-8 -*-
 import sys
 import commands
+import os
+
+
+def get_jars(dir_path):
+    jars = []
+
+    if os.path.exists(dir_path):
+        for name in os.listdir(dir_path):
+            file_path = os.path.join(dir_path, name)
+
+            if os.path.isfile(file_path):
+                if file_path.endswith(".jar"):
+                    jars.append(file_path)
+            elif os.path.isdir(file_path):
+                jars.extend(get_jars(file_path))
+
+    return jars
+
 
 user = 'root'
 
 java_home = '/usr/java/default'
 
-classpaths = [
-              '/data0/workspace/cosmos/conf/',
-              '/data0/workspace/cosmos/node/target/cosmos-node-0.1.jar',
-              '/data0/workspace/cosmos/node/target/cosmos-node-0.1-lib/*']
+project_home = '/data0/workspace/cosmos'
+
+module_home = project_home + '/node'
+
+classpaths = [project_home + '/conf']
+classpaths.extend(get_jars(module_home))
 
 main_class = 'com.weibo.dip.cosmos.node.client.ConsoleClient'
 
+
 if __name__ == '__main__':
-    command = 'sudo -u %s %s/bin/java -cp %s %s %s' % (user, java_home, ':'.join(classpaths), main_class, ' '.join(sys.argv[1:]))
+    command = 'sudo -u %s %s/bin/java -cp %s %s %s' % (
+        user, java_home, ':'.join(classpaths), main_class, ' '.join(sys.argv[1:]))
 
     print commands.getoutput(command)
