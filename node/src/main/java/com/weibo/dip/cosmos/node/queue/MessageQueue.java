b5c7c0e2a15d7b7e909d009d8750daa999b5b60e
zhiqiang32 <zhiqiang32@staff.weibo.com>
2019-05-20 14:56:50 +0800

    improved scheduling of queue resources

--- node/src/main/java/com/weibo/dip/cosmos/node/queue/MessageQueue.java
+++ node/src/main/java/com/weibo/dip/cosmos/node/queue/MessageQueue.java
@@ -109,6 +109,7 @@ public class MessageQueue {
 
           String deleteSql = "delete from message_queue where consumer = ?";
 
+          // 没有判断
           queryRunner.update(conn, deleteSql, consumer);
         }
 
@@ -200,4 +201,24 @@ public class MessageQueue {
 
     return queryRunner.query(sql, new StringHandler());
   }
+
+
+
+  /**
+   * Get next fire message from specified queue.
+   *
+   * @return message
+   * @throws Exception if access db error
+   */
+  public Message getNextFireMessageByQueue(String queueName,Date timestamp) throws Exception {
+
+    String sql = "select * from message_queue where queue = ？and msgtimestamp <= ? order by priority asc, msgtimestamp asc limit 1";
+
+    List<Message> messages = queryRunner.query(sql, new MessageHandler(), queueName,timestamp);
+
+    return CollectionUtils.isNotEmpty(messages) ? messages.get(0) : null;
+
+  }
+
+
 }
